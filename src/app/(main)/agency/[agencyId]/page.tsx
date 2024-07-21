4db8201b5ca7bbc4e2f09ca168ddd5b5f40a9428
VANSH AGARWAL <vanshagarwal11@gmail.com>
2024-07-22 02:57:57 +0530

    Make agency-detail form

---
+++ src/app/(main)/agency/[agencyId]/page.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const page = ({params}: {params: {agencyId: string}}) => {
+  return (
+    <div>
+      {params.agencyId}
+    </div>
+  )
+}
+
+export default page
--- src/components/site/navigation/index.tsx
+++ src/components/site/navigation/index.tsx
@@ -12,7 +12,7 @@ type Props = {
 
 const Navigation = ({ user }: Props) => {
     return (
-        <div className="p-4 flex items-center justify-between relative">
+        <div className="relative top-0 right-0 left-0 p-4 flex items-center justify-between z-10">
             <aside className="flex items-center gap-2">
                 <Image
                     src={'./assets/plura-logo.svg'}
