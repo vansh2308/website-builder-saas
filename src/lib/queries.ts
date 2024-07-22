fed3fc43d0bdfae33997ac4ec97e87c9153d4d50
VANSH AGARWAL <vanshagarwal11@gmail.com>
2024-07-22 19:12:18 +0530

    Fix CommandItem active states

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -294,10 +294,10 @@ export const upsertAgency = async (agency: Agency, price?: Plan) => {
 
 
 export const getNotificationAndUser = async (agencyId: string) => {
-    try{
+    try {
         const response = await db.notification.findMany({
-            where: {agencyId},
-            include: {User: true},
+            where: { agencyId },
+            include: { User: true },
             orderBy: {
                 createdAt: "desc"
             }
@@ -305,7 +305,7 @@ export const getNotificationAndUser = async (agencyId: string) => {
         })
 
         return response
-    } catch(error){
+    } catch (error) {
         console.log(error)
     }
 }
@@ -318,108 +318,108 @@ export const getNotificationAndUser = async (agencyId: string) => {
 export const upsertSubAccount = async (subAccount: SubAccount) => {
     if (!subAccount.companyEmail) return null
     const agencyOwner = await db.user.findFirst({
-      where: {
-        Agency: {
-          id: subAccount.agencyId,
+        where: {
+            Agency: {
+                id: subAccount.agencyId,
+            },
+            role: 'AGENCY_OWNER',
         },
-        role: 'AGENCY_OWNER',
-      },
     })
     if (!agencyOwner) return console.log('🔴Erorr could not create subaccount')
     const permissionId = v4()
     const response = await db.subAccount.upsert({
-      where: { id: subAccount.id },
-      update: subAccount,
-      create: {
-        ...subAccount,
-        Permissions: {
-          create: {
-            access: true,
-            email: agencyOwner.email,
-            id: permissionId,
-          },
-          connect: {
-            subAccountId: subAccount.id,
-            id: permissionId,
-          },
+        where: { id: subAccount.id },
+        update: subAccount,
+        create: {
+            ...subAccount,
+            Permissions: {
+                create: {
+                    access: true,
+                    email: agencyOwner.email,
+                    id: permissionId,
+                },
+                connect: {
+                    subAccountId: subAccount.id,
+                    id: permissionId,
+                },
+            },
+            Pipeline: {
+                create: { name: 'Lead Cycle' },
+            },
+            SidebarOption: {
+                create: [
+                    {
+                        name: 'Launchpad',
+                        icon: 'clipboardIcon',
+                        link: `/subaccount/${subAccount.id}/launchpad`,
+                    },
+                    {
+                        name: 'Settings',
+                        icon: 'settings',
+                        link: `/subaccount/${subAccount.id}/settings`,
+                    },
+                    {
+                        name: 'Funnels',
+                        icon: 'pipelines',
+                        link: `/subaccount/${subAccount.id}/funnels`,
+                    },
+                    {
+                        name: 'Media',
+                        icon: 'database',
+                        link: `/subaccount/${subAccount.id}/media`,
+                    },
+                    {
+                        name: 'Automations',
+                        icon: 'chip',
+                        link: `/subaccount/${subAccount.id}/automations`,
+                    },
+                    {
+                        name: 'Pipelines',
+                        icon: 'flag',
+                        link: `/subaccount/${subAccount.id}/pipelines`,
+                    },
+                    {
+                        name: 'Contacts',
+                        icon: 'person',
+                        link: `/subaccount/${subAccount.id}/contacts`,
+                    },
+                    {
+                        name: 'Dashboard',
+                        icon: 'category',
+                        link: `/subaccount/${subAccount.id}`,
+                    },
+                ],
+            },
         },
-        Pipeline: {
-          create: { name: 'Lead Cycle' },
-        },
-        SidebarOption: {
-          create: [
-            {
-              name: 'Launchpad',
-              icon: 'clipboardIcon',
-              link: `/subaccount/${subAccount.id}/launchpad`,
-            },
-            {
-              name: 'Settings',
-              icon: 'settings',
-              link: `/subaccount/${subAccount.id}/settings`,
-            },
-            {
-              name: 'Funnels',
-              icon: 'pipelines',
-              link: `/subaccount/${subAccount.id}/funnels`,
-            },
-            {
-              name: 'Media',
-              icon: 'database',
-              link: `/subaccount/${subAccount.id}/media`,
-            },
-            {
-              name: 'Automations',
-              icon: 'chip',
-              link: `/subaccount/${subAccount.id}/automations`,
-            },
-            {
-              name: 'Pipelines',
-              icon: 'flag',
-              link: `/subaccount/${subAccount.id}/pipelines`,
-            },
-            {
-              name: 'Contacts',
-              icon: 'person',
-              link: `/subaccount/${subAccount.id}/contacts`,
-            },
-            {
-              name: 'Dashboard',
-              icon: 'category',
-              link: `/subaccount/${subAccount.id}`,
-            },
-          ],
-        },
-      },
     })
     return response
-  }
+}
 
-  export const getUserPermissions = async (userId: string) => {
+export const getUserPermissions = async (userId: string) => {
     const response = await db.user.findUnique({
-      where: { id: userId },
-      select: { Permissions: { include: { SubAccount: true } } },
+        where: { id: userId },
+        select: { Permissions: { include: { SubAccount: true } } },
     })
-  
+
     return response
-  }
-  
+}
+
 
 
 export const updateUser = async (user: Partial<User>) => {
     const response = await db.user.update({
-      where: { email: user.email },
-      data: { ...user },
+        where: { email: user.email },
+        data: { ...user },
     })
-  
+
     await clerkClient.users.updateUserMetadata(response.id, {
-      privateMetadata: {
-        role: user.role || 'SUBACCOUNT_USER',
-      },
+        privateMetadata: {
+            role: user.role || 'SUBACCOUNT_USER',
+        },
     })
-  
+
     return response
-  }
+}
 
 
 
@@ -428,19 +428,40 @@ export const changeUserPermissions = async (
     userEmail: string,
     subAccountId: string,
     permission: boolean
-  ) => {
+) => {
     try {
-      const response = await db.permissions.upsert({
-        where: { id: permissionId },
-        update: { access: permission },
-        create: {
-          access: permission,
-          email: userEmail,
-          subAccountId: subAccountId,
-        },
-      })
-      return response
+        const response = await db.permissions.upsert({
+            where: { id: permissionId },
+            update: { access: permission },
+            create: {
+                access: permission,
+                email: userEmail,
+                subAccountId: subAccountId,
+            },
+        })
+        return response
     } catch (error) {
-      console.log('🔴Could not change persmission', error)
+        console.log('🔴Could not change persmission', error)
     }
+}
+
+
+
+export const deleteSubAccount = async (subaccountId: string) => {
+    const response = await db.subAccount.delete({
+        where: {
+            id: subaccountId,
+        },
+    })
+    return response
+}
+
+
+export const getSubaccountDetails = async (subaccountId: string) => {
+    const response = await db.subAccount.findUnique({
+      where: {
+        id: subaccountId,
+      },
+    })
+    return response
   }
